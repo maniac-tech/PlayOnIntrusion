de084e31f1671f3811757a2ffb079a65fa20e609
maniac-tech <ajj_2396@yahoo.com>
2017-08-12 22:42:10 +0530

    created object of connect thread and added discovery, establish, and intent

--- app/src/main/java/com/example/maniac/arduinobluetooth/MainActivity.java
+++ app/src/main/java/com/example/maniac/arduinobluetooth/MainActivity.java
@@ -4,30 +4,19 @@ package com.example.maniac.arduinobluetooth;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Adapter;
 import android.widget.Toast;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,9 +26,11 @@ public class MainActivity extends AppCompatActivity {
     //defining Bluetooth Device:
     BluetoothDevice device;
 
-    String arrayDeviceName [] = new String [10];
-    String arrayDeviceAdd [] = new String [10];
-    String del [] = {"1","2","3"};
+    private static final String TAG = "Debug";
+    private Handler mHandler; // handler that gets info from Bluetooth service
+
+
+    //ConnectedThread data = new ConnectedThread(client.mmSocket);
 
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -52,14 +43,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        managePermissions();
 
-        //Checking for Permission on RUNTIME:
+
+    }
+
+    //Funtion responsible for checking Permissions on Runtime:
+    public void managePermissions(){
+        //Managing Bluetooth permissions on RunTime:
         int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
                 MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
 
-        // Check if we have write permission
+        //Managing External Storage permissions on RunTime:
         int permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
         if (permission != PackageManager.PERMISSION_GRANTED) {
@@ -72,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Function responsible for Attempting Discovery of nearby Bluetooth Devices:
     public void discoverDevices(View view){
         //Intent to capture states:
         IntentFilter filter = new IntentFilter();
@@ -84,37 +82,20 @@ public class MainActivity extends AppCompatActivity {
 
         //Starting to discover:
         adapter.startDiscovery();
+    }
 
-        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+    //Function responsible for establishing connection to nearby Bluetooth Devices:
+    public void establishClientConnection(View view){
+        Log.d("Debug:","Cancelling Discovery before attempt connection");
+        adapter.cancelDiscovery();
+        Log.d("Debug","establishClientConnection complete");
 
-//        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.listview,arrayDeviceName);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.listview,del);
-        ListView listView = (ListView) findViewById(R.id.listView);
-
-        int i=0;
-        if (pairedDevices.size() > 0) {
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices) {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                arrayDeviceName [i] = deviceName;
-                arrayDeviceAdd [i] = deviceHardwareAddress;
-                Log.d(deviceName,deviceHardwareAddress);
-                i++;
-            }
-        }
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent toConnect = new Intent(MainActivity.this,DeviceSummary.class);
-                toConnect.putExtra("dName",arrayDeviceName[i]);
-                toConnect.putExtra("dAdd",arrayDeviceAdd[i]);
-                startActivity(toConnect);
-            }
-
-        });
+        Log.d("Debug","ConnectThread object ready");
+        Intent switchScreen = new Intent(MainActivity.this,DeviceSummary.class);
+        ConnectThread client = new ConnectThread(device);
+        client.run();
+        //data.run();
+        startActivity(switchScreen);
     }
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -141,111 +122,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void showToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
-
-//    public void establishClientConnection(){
-//        Log.d("Debug:","Cancelling Discovery before attempt connection");
-//        adapter.cancelDiscovery();
-//        Log.d("Debug","establishClientConnection complete");
-//        ConnectThread client = new ConnectThread(device);
-//        Log.d("Debug","ConnectThread object ready");
-//        client.run();
-//    }
-
-
-    //Managing Connections:
-
-    private static final String TAG = "Debug";
-    private Handler mHandler; // handler that gets info from Bluetooth service
-
-    private interface MessageConstants {
-        public static final int MESSAGE_READ = 0;
-        public static final int MESSAGE_WRITE = 1;
-        public static final int MESSAGE_TOAST = 2;
-
-        // ... (Add other message types here as needed.)
-    }
-
-    private class ConnectedThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-        private byte[] mmBuffer; // mmBuffer store for the stream
-
-        public ConnectedThread(BluetoothSocket socket) {
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-
-            // Get the input and output streams; using temp objects because
-            // member streams are final.
-            try {
-                tmpIn = socket.getInputStream();
-            } catch (IOException e) {
-                Log.e(TAG, "Error occurred when creating input stream", e);
-            }
-            try {
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) {
-                Log.e(TAG, "Error occurred when creating output stream", e);
-            }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run() {
-            mmBuffer = new byte[1024];
-            int numBytes; // bytes returned from read()
-
-            // Keep listening to the InputStream until an exception occurs.
-            while (true) {
-                try {
-                    // Read from the InputStream.
-                    numBytes = mmInStream.read(mmBuffer);
-                    // Send the obtained bytes to the UI activity.
-                    Message readMsg = mHandler.obtainMessage(
-                            MessageConstants.MESSAGE_READ, numBytes, -1,
-                            mmBuffer);
-                    readMsg.sendToTarget();
-                } catch (IOException e) {
-                    Log.d(TAG, "Input stream was disconnected", e);
-                    break;
-                }
-            }
-        }
-
-        // Call this from the main activity to send data to the remote device.
-        public void write(byte[] bytes) {
-            try {
-                mmOutStream.write(bytes);
-
-                // Share the sent message with the UI activity.
-                Message writtenMsg = mHandler.obtainMessage(
-                        MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer);
-                writtenMsg.sendToTarget();
-            } catch (IOException e) {
-                Log.e(TAG, "Error occurred when sending data", e);
-
-                // Send a failure message back to the activity.
-                Message writeErrorMsg =
-                        mHandler.obtainMessage(MessageConstants.MESSAGE_TOAST);
-                Bundle bundle = new Bundle();
-                bundle.putString("toast",
-                        "Couldn't send data to the other device");
-                writeErrorMsg.setData(bundle);
-                mHandler.sendMessage(writeErrorMsg);
-            }
-        }
-
-        // Call this method from the main activity to shut down the connection.
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Could not close the connect socket", e);
-            }
-        }
     }
 
 }
